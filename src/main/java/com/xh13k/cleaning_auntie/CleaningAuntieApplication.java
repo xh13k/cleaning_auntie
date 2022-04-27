078d2e9aad7ec70e3f6319ce0e2f9ee74b554616
xh13k <1005428571@qq.com>
2022-04-27 12:55:12 +0800

    Initial commit

---
+++ src/main/java/com/xh13k/cleaning_auntie/CleaningAuntieApplication.java
@@ -0,0 +1,13 @@
+package com.xh13k.cleaning_auntie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningAuntieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleaningAuntieApplication.class, args);
+    }
+
+}
